fad8d7a41b7f70e9717b4f01606a2532293a4cfb
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-02-12 14:57:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Cities from "./components/Cities";
 
 function App() {
   return (
-    <div>
-      <Landing></Landing>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/" component={Cities} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
