cbebd301c3102af17c03849bd64c392d259cbc4a
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-02-12 12:56:21 +0100

    commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>QueerMeUp</h1>
-        <p>
-          Find the perfect queer activities, bars, shows and events in your
-          favourite city
-        </p>
-        <div className="container text-center">
-          <a class="rainbow rainbow-5">Sign In!</a>
+        <div className="topLeft">
+          <h1 className="">QueerMeUp</h1>
+        </div>
+        <div className="">
+          <p>
+            Find the perfect queer activities, bars, shows and events in your
+            favourite city
+          </p>
 
-          <a class="rainbow rainbow-5">Log In!</a>
-
-          {/* <Button variant="dark" className="signInButton" size="lg">
-            Sign In
-          </Button>
-          {/* <p>Or, If you are already a queer member:</p> */}
-          {/*<Button variant="dark" className="logInButton" size="lg">
-            Log In
-          </Button> */}
+          <div className="container text-center">
+            <a class="rainbow rainbow-5">Sign Up!</a>
+            <a class="rainbow rainbow-5">Log In!</a>
+          </div>
         </div>
       </div>
     );
