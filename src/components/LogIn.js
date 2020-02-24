63a402e2e5461a53d75a3b2cd0c8fd0acd964cc2
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-02-24 16:36:33 +0100

    log in sign up

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class LogIn extends Component {
   render() {
-    return <div>Log In</div>;
+    return (
+      <div>
+        <div className="PageSwitcher">
+          <a
+            to="/Login"
+            activeClassName="PageSwitcher__Item--Active"
+            className="PageSwitcher__Item"
+          >
+            Log In
+          </a>
+          <Link
+            to="/SignUp"
+            activeClassName="PageSwitcher__Item--Active"
+            className="PageSwitcher__Item"
+          >
+            Sign Up
+          </Link>
+        </div>
+        <h1>Log In</h1>
+      </div>
+    );
   }
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class SignUp extends Component {
   state = {
@@ -15,9 +16,9 @@ export default class SignUp extends Component {
     return (
       <div>
         <div className="PageSwitcher">
-          <a href="#" className="PageSwitcher__Item">
+          <Link to="/Login" className="PageSwitcher__Item">
             Log In
-          </a>
+          </Link>
           <a
             href="#"
             activeClassName="PageSwitcher__Item--Active"
@@ -25,7 +26,66 @@ export default class SignUp extends Component {
           >
             Sign Up
           </a>
-        </div>{" "}
+        </div>
+        <h1>Sign Up</h1>
+        <div className="FormCenter">
+          <form onSubmit={this.handleSubmit} className="FormFields">
+            <div className="FormField">
+              <label className="FormField__Label" htmlFor="name">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="FormField__Input"
+                placeholder="Enter your full name"
+                name="name"
+              />
+            </div>
+            <div className="FormField">
+              <label className="FormField__Label" htmlFor="email">
+                Email adress
+              </label>
+              <input
+                type="email"
+                id="name"
+                className="FormField__Input"
+                placeholder="Enter your email adress"
+                name="email"
+              />
+            </div>
+            <div className="FormField">
+              <label className="FormField__Label" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="FormField__Input"
+                placeholder="Enter your password"
+                name="password"
+              />
+            </div>
+            <div className="FormField">
+              <label className="FormField__Label" htmlFor="picture">
+                Picture
+              </label>
+              <input
+                type="text"
+                id="picture"
+                className="FormField__Input"
+                placeholder="Enter your picture url"
+                name="picture"
+              />
+            </div>
+            <div className="FormField">
+              <button className="FormField__Button mr-20">Sign Up</button>{" "}
+              <a to="/sign-in" className="FormField__Link">
+                I'm already member
+              </a>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
