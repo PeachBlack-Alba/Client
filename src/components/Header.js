fa0a8562bbf7fb98236bacf94a77e7cd950bd865
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-03-06 16:41:33 +0100

    some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="containerHeader">
         <Link
           className="logout"
           to="/"
