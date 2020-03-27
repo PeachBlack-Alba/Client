6241a45704dbf3d87a23463f7005a0afba686822
PeachBlack-Alba <57803942+PeachBlack-Alba@users.noreply.github.com>
2020-03-27 16:57:36 +0100

    css

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Container>
+        <Container className="container">
           <div className="App">
             <Switch>
               <Route exact path="/" component={Landing} />
